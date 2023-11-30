147051c1c600aba001b725c1d960d83763b69dcf
intec20708 <145434459+intec20708@users.noreply.github.com>
2023-11-30 17:30:25 +0900

    Update main3.py

--- code/main3.py
+++ code/main3.py
@@ -18,18 +18,12 @@ while True:
     
     for det in dets:
         shape = predictor(img, det)
-        
-        # for i, point in enumerate(shape.parts()):
-        #     cv2.circle(img, center=(point.x, point.y), radius=2, color=(0, 0, 255), thickness=-1)
-        #     cv2.putText(img, text=str(i), org=(point.x, point.y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=(255, 255, 255), thickness=2)
-            
+          
         try:
             x1 = det.left()
             y1 = det.top()
             x2 = det.right()
             y2 = det.bottom()
-            
-            # cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), color=(255, 0, 0), thickness=2)
             
             center_x = shape.parts()[4].x 
             center_y = shape.parts()[4].y - 7
